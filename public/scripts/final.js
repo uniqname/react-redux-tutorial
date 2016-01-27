c4feab07b72cb437ba749ca00251e38d6e0a19b7
Tim Riker <Tim@Rikers.org>
2016-01-27 11:46:51 -0700

    need to add it in both places?

--- public/scripts/final.js
+++ public/scripts/final.js
@@ -2,7 +2,8 @@ let comments = [
     {id: 1, author: "Cory Brown", text: "My 2 scents"},
     {id: 2, author: "Jared Anderson", text: "Let me put it this way. You've heard of Socrates? Aristotle? Plato? Morons!"},
     {id: 3, author: "Matt Poulson", text: "It's just a function!"},
-    {id: 4, author: "Bruce Campbell", text: "Fish in a tree? How can that be?"}
+    {id: 4, author: "Bruce Campbell", text: "Fish in a tree? How can that be?"},
+    {id: 5, author: "Tim Riker", text: "So what we're learning is to keep our componets dumb?"}
 ];
 
 const Comment = (props) => (
